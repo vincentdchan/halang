62cdf4b25b9aaa242da23a88d878fb81ef262695
Vincent Chan <cdzos97@gmail.com>
2017-12-07 00:00:25 +0800

    midprog generator

--- MidProg.cpp
+++ MidProg.cpp
@@ -1,20 +1,26 @@
+#include "halang.h"
 #include "parser.h"
 #include "MidProg.h"
 #include "util.h"
 #include <string>
+#include <sstream>
 
 namespace halang {
 
     void MidProg::Visit(Node * node) {
-        // wait to finish
+        node->Visit(this);
     }
 
     void MidProg::Visit(ProgramNode * node) {
-        // wait to finish
+        for (auto i = node->statements.begin();
+            i != node->statements.end(); i++) {
+                Visit(*i);
+            }
     }
 
     void MidProg::Visit(NumberNode * node) {
-        // wait to finish
+        U16String str = utils::utf8_to_utf16(std::to_string(node->number));
+        names_stack.push(str);
     }
 
     void MidProg::Visit(NullStatementNode * node) {
@@ -22,7 +28,7 @@ namespace halang {
     }
 
     void MidProg::Visit(IdentifierNode * node) {
-        // wait to finish
+        names_stack.push(node->name);
     }
 
     void MidProg::Visit(StringNode * node) {
@@ -31,10 +37,15 @@ namespace halang {
 
     void MidProg::Visit(LetStatementNode * node) {
         // wait to finish
+        for (auto i = node->assignments.begin();
+            i != node->assignments.end(); i++) {
+            Visit(*i);
+        }
     }
 
     void MidProg::Visit(ExpressionStatementNode * node) {
         // wait to finish
+        Visit(node->expression);
     }
 
     void MidProg::Visit(IfStatementNode * node) {
@@ -70,7 +81,19 @@ namespace halang {
     }
 
     void MidProg::Visit(AssignExpressionNode * node) {
-        // wait to finish
+        Code code;
+        code.code = u"MOV";
+
+        Visit(node->identifier);
+        code.op1 = names_stack.top();
+        names_stack.pop();
+
+        Visit(node->expression);
+        code.op2 = names_stack.top();
+        names_stack.pop();
+
+        names_stack.push(code.op1);
+        codes.push_back(code);
     }
 
     void MidProg::Visit(ListExpressionNode * node) {
@@ -78,11 +101,64 @@ namespace halang {
     }
 
     void MidProg::Visit(UnaryExpressionNode * node) {
-        // wait to finish
+        Code code;
+        Visit(node->child);
+
+        auto tmp = GetTmpVariableName();
+        code.code = u"MUL";
+        code.op1 = tmp;
+        code.op3 = names_stack.top();
+        names_stack.pop();
+
+        switch(node->op) {
+            case OperatorType::ADD:
+                code.op2 = u"1";
+                break;
+            case OperatorType::SUB:
+                code.op2 = u"-1";
+                break;
+        }
+        
+        names_stack.push(tmp);
+        codes.push_back(code);
     }
 
     void MidProg::Visit(BinaryExpressionNode * node) {
-        // wait to finish
+        Code code;
+        Visit(node->left);
+        Visit(node->right);
+
+        code.op3 = names_stack.top();
+        names_stack.pop();
+
+        code.op2 = names_stack.top();
+        names_stack.pop();
+
+        U16String tmp = GetTmpVariableName();
+        code.op1 = tmp;
+
+        switch(node->op) {
+            case OperatorType::ADD:
+                code.code = u"ADD";
+                break;
+            case OperatorType::SUB:
+                code.code = u"SUB";
+                break;
+            case OperatorType::MUL:
+                code.code = u"MUL";
+                break;
+            case OperatorType::DIV:
+                code.code = u"DIV";
+                break;
+            case OperatorType::MOD:
+                code.code = u"MOD";
+                break;
+            case OperatorType::POW:
+                code.code = u"POW";
+        }
+
+        names_stack.push(tmp);
+        codes.push_back(code);
     }
 
     void MidProg::Visit(DoExpressionNode * node) {
@@ -91,6 +167,19 @@ namespace halang {
 
     void MidProg::Visit(FunExpressionNode * node) {
         // wait to finish
+    }
+
+    bool MidProg::IsIdentifier(Node * node) const {
+        return node->AsIdentifier() != nullptr;
+    }
+
+    const std::vector<Code>&
+    MidProg::GetCodes() const {
+        return codes;
+    }
+
+    bool MidProg::IsNumber(Node * node) const {
+        return node->AsNumber() != nullptr;
     }
 
     U16String MidProg::GetTmpVariableName() {
@@ -126,6 +215,19 @@ int main(int argc, char * argv[]) {
 
         MidProg midProg;
         midProg.Visit(root);
+
+        for (auto i = midProg.GetCodes().begin();
+            i != midProg.GetCodes().end(); i++) {
+                std::cout << utils::utf16_to_utf8(i->code) 
+                << "\t"
+                << utils::utf16_to_utf8(i->op1) 
+                << "\t" 
+                << utils::utf16_to_utf8(i->op2) 
+                << "\t"
+                << utils::utf16_to_utf8(i->op3) 
+                << std::endl;
+            }
+
     } else {
         for (auto i = parser.getMessages().begin();
         i != parser.getMessages().end(); i++) {
--- MidProg.h
+++ MidProg.h
@@ -3,8 +3,24 @@
 #include "ast.h"
 #include "visitor.h"
 #include <unordered_set>
+#include <stack>
+#include <vector>
 
 namespace halang {
+
+    class Code {
+    public:
+
+        Code() { }
+        Code(U16String o1, U16String o2, U16String o3) :
+        op1(o1), op2(o2), op3(o3) { }
+
+        U16String code;
+        U16String op1;
+        U16String op2;
+        U16String op3;
+
+    };
 
     class MidProg : public Visitor {
     public:
@@ -14,13 +30,23 @@ namespace halang {
 		NODE_LIST(VISIT_METHOD)
 #undef VISIT_METHOD
 
+        const std::vector<Code>&
+        GetCodes() const;
+
+
     protected:
 
         U16String GetTmpVariableName();
 
+        bool IsNumber(Node * node) const;
+        bool IsIdentifier(Node * node) const;
+
     private:
         std::unordered_set<U16String> var_names_set;
         int tmp_name_counter = 0;
+
+        std::stack<U16String> names_stack;
+        std::vector<Code> codes;
 
    };
 
